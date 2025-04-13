9cf0e24e46d903fb2495107ccf41ef2ce5f1305d
128atharvask <128atharvakulkarni@gmail.com>
2025-04-14 01:48:30 +0530

    Trying memstreamer after this

--- nttAccel/src/main/scala/DummyRoCC.scala
+++ nttAccel/src/main/scala/DummyRoCC.scala
@@ -5,12 +5,79 @@ import chisel3.util._
 import freechips.rocketchip.tile._
 import org.chipsalliance.cde.config._
 import freechips.rocketchip.diplomacy._
-import freechips.rocketchip.rocket.{HellaCacheReq, M_XRD, M_XWR}
+import freechips.rocketchip.rocket.{TLBConfig}
+import freechips.rocketchip.subsystem.{SystemBusKey}
+import freechips.rocketchip.rocket.constants.MemoryOpConstants
+import roccaccutils._
 
-// Main RoCC accelerator class
-class DummyRoCC(opcodes: OpcodeSet)(implicit p: Parameters) extends LazyRoCC(
+case object DummyAccelTLB extends Field[Option[TLBConfig]](None)
+
+class DummyRoCC(opcodes: OpcodeSet)(implicit p: Parameters) extends MemStreamerAccel(
     opcodes = opcodes) {
-  override lazy val module = new DummyImp(this)
+  override lazy val module = new DummyRoCCImp(this)
+
+  lazy val tlbConfig = p(DummyAccelTLB).get
+  lazy val xbarBetweenMem = false // No need for xbar in this simple example
+  lazy val logger = NTTLogger
+}
+
+// Memory streaming implementation for 16-bit value copy
+class DummyStreamer extends MemStreamer {
+  val io = IO(new Bundle {
+    val mem_stream = Flipped(new MemLoaderConsumerIO)
+    val memwrites_in = new MemWriterIO
+    val src_addr = Input(UInt(64.W))
+    val dst_addr = Input(UInt(64.W))
+    val start = Input(Bool())
+    val busy = Output(Bool())
+  })
+
+  val busy = RegInit(false.B)
+  io.busy := busy
+
+  // Buffer for the value being copied
+  val buffer = RegInit(0.U(16.W))
+
+  // Simple state machine
+  when (io.start && !busy) {
+    busy := true.B
+  }
+
+  // Handle memory reads
+  when (busy && io.mem_stream.output.valid) {
+    buffer := io.mem_stream.output.bits.data(15, 0)
+    io.memwrites_in.valid := true.B
+    io.memwrites_in.data := buffer
+    io.memwrites_in.addr := io.dst_addr
+    io.memwrites_in.size := 1.U // 2 bytes
+    busy := false.B
+  } .otherwise {
+    io.memwrites_in.valid := false.B
+    io.memwrites_in.data := 0.U
+    io.memwrites_in.addr := 0.U
+    io.memwrites_in.size := 0.U
+  }
+
+  io.mem_stream.output.ready := busy
+}
+
+class DummyRoCCImp(outer: DummyRoCC)(implicit p: Parameters) extends MemStreamerAccelImp(outer) {
+  
+  lazy val queueDepth = 2 // Small queue depth since we only do single transfers
+
+  lazy val cmd_router = Module(new CommandRouter(queueDepth))
+  lazy val streamer = Module(new DummyStreamer())
+
+  // Connect command router to streamer
+  streamer.io.src_addr := cmd_router.io.src_info.addr
+  streamer.io.dst_addr := cmd_router.io.dest_info.addr
+  streamer.io.start := cmd_router.io.src_info.valid
+  cmd_router.io.bufs_completed := !streamer.io.busy
+  cmd_router.io.no_writes_inflight := !streamer.io.memwrites_in.valid
+
+  // Connect memory interfaces
+  streamer.io.mem_stream <> memloader.io.consumer
+  memwriter.io.memwrites_in <> streamer.io.memwrites_in
 }
 
 // Config class to add the accelerator to Chipyard config
@@ -22,117 +89,5 @@ class WithDummyRoCC extends Config((site, here, up) => {
       dummy
     }
   )
-})
-
-// Implementation of the dummy accelerator
-class DummyImp(outer: DummyRoCC)(implicit p: Parameters) extends LazyRoCCModuleImp(outer) {
-  // RoCC Interface
-  io.resp.valid := false.B
-  
-  // State mapping
-  object State {
-    val idle = 0.U(3.W)    // Waiting for command
-    val reading = 1.U(3.W) // Reading from source
-    val waiting = 2.U(3.W) // Waiting for response
-    val writing = 3.U(3.W) // Writing to destination
-    val done = 4.U(3.W)    // Operation complete
-  }
-  import State._
-  
-  // State register
-  val state = RegInit(idle)
-  io.busy := (state =/= idle)
-
-  // Command handling
-  val cmd = io.cmd
-  val rs1 = cmd.bits.rs1  // Source address
-  val rs2 = cmd.bits.rs2  // Destination address
-
-  // Store status for memory requests
-  val status = RegEnable(io.cmd.bits.status, io.cmd.fire)
-
-  // Buffer for the single element and request tracking
-  val buffer = RegInit(0.U(16.W))
-  val req_tag = RegInit(0.U(5.W))
-
-  // Memory request tracking
-  val req_valid = RegInit(false.B)
-  val req_rdy = Wire(Bool())
-  val mem_addr = RegInit(0.U(64.W))
-  val mem_cmd = RegInit(M_XRD)
-  val mem_data = RegInit(0.U(64.W))
-
-  // Cache interface connection
-  def connectHellaCache(req: DecoupledIO[HellaCacheReq]): Unit = {
-    req.valid := req_valid
-    req_rdy := req.ready
-    req.bits.tag := req_tag
-    req.bits.addr := mem_addr
-    req.bits.cmd := mem_cmd
-    req.bits.size := 1.U  // 2 bytes = log2(2) = 1
-    req.bits.data := mem_data
-    req.bits.signed := false.B
-    req.bits.dprv := status.dprv
-    req.bits.dv := status.dv
-    req.bits.phys := false.B
-  }
-
-  // Connect to cache
-  connectHellaCache(io.mem.req)
-
-  val mem_resp_valid = io.mem.resp.valid
-  val mem_resp_tag = io.mem.resp.bits.tag
-  val mem_resp_data = io.mem.resp.bits.data
-  val mem_resp_valid_reg = RegNext(mem_resp_valid)
-  val mem_resp_tag_reg = RegNext(mem_resp_tag)
-
-
-  // State machine logic
-  switch(state) {
-    is(idle) {
-      when(cmd.valid) {
-        state := reading
-      }
-    }
-
-    is(reading) {
-      req_valid := true.B
-      mem_addr := rs1
-      mem_cmd := M_XRD
-      req_tag := 0.U
-      when(req_rdy && req_valid) {
-        state := waiting
-        req_valid := false.B
-      }.otherwise {
-        state := reading
-      }
-    }
-
-    is(waiting) {
-      when(mem_resp_valid && mem_resp_tag === req_tag) {
-        buffer := mem_resp_data
-        state := writing
-      }.otherwise {
-        state := waiting
-      }
-    }
-
-    is(writing) {
-      req_valid := true.B
-      mem_addr := rs2
-      mem_cmd := M_XWR
-      req_tag := 1.U
-      when(mem_resp_valid && mem_resp_tag === req_tag) {
-        req_valid := false.B
-        state := done
-      }
-    }
-
-    is(done) {
-      state := idle
-    }
-  }
-
-  // Command ready logic
-  io.cmd.ready := (state === idle)
-} 
+  case DummyAccelTLB => Some(TLBConfig(nSets = 4, nWays = 4, nSectors = 1, nEntries = 4))
+}) 
---
+++ nttAccel/src/main/scala/util.scala
@@ -0,0 +1,24 @@
+package nttAccel
+
+import chisel3._
+import chisel3.util._
+import chisel3.{Printable}
+import freechips.rocketchip.tile._
+import org.chipsalliance.cde.config._
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.rocket.{TLBConfig}
+import freechips.rocketchip.util.DecoupledHelper
+import freechips.rocketchip.rocket.constants.MemoryOpConstants
+
+object NTTLogger {
+  def logInfo(format: String, args: Bits*)(implicit p: Parameters): Unit = {
+    val loginfo_cycles = RegInit(0.U(64.W))
+    loginfo_cycles := loginfo_cycles + 1.U
+    printf("cy: %d, ", loginfo_cycles)
+    printf(Printable.pack(format, args:_*))
+  }
+
+  def logWaveStyle(format: String, args: Bits*)(implicit p: Parameters): Unit = {
+    // Empty implementation for now
+  }
+}
